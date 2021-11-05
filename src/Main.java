1ad3bd70ff0d0e4ae9888be96c7395bdd5e8fce6
Kevin BIOJOUT <biojoutk1106@gmail.com>
2021-11-05 19:08:19 +0100

    feat: Main class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    /**
+     * Démarre l'application de visualisation d'un réseau social représenté à
+     * l'aide de graphes.
+     * @param args Arguments de la ligne de commande.
+     */
+    public static void main(String[] args) {
+        // À faire: Initialisation GUI
+    }
+}
